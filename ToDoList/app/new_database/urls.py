0f2df792a6fc9f08112d38b804057da0e962b711
Ivan Rastorguev <ivan.rastorguev.99@mail.ru>
2024-04-09 00:06:26 +0700

    prepared project for delivery

--- ToDoList/app/new_database/urls.py
+++ ToDoList/app/new_database/urls.py
@@ -7,18 +7,14 @@ task_template = APIRouter()
 templates = Jinja2Templates(directory='templates')
 
 @task_template.get('/')
-def index(request: Request):
+def show_homepage(request: Request):
 	tasks:dict = {"tasks": tasks_list_point(request)}
 	return templates.TemplateResponse(request=request, name='home_page.html', context=tasks)
 
 @task_template.get('/about')
-def index(request: Request):
+def show_aboutpage(request: Request):
 	return templates.TemplateResponse(request=request, name='about_page.html')
 
 @task_template.get('/autorisation')
-def index(request: Request):
+def show_autorisation(request: Request):
 	return templates.TemplateResponse(request=request, name='auto_page.html')
-
-@task_template.get('/create')
-def creating_page(request: Request):
-	return templates.TemplateResponse(request=request, name='task_create.html')
